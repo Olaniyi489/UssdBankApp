6e3bf8a83f14776a520043e8b71b84630fafde41
Olaniyi489 <olawareez96@gmail.com>
2022-10-13 14:33:22 +0100

    implementation of new methods

--- UssdBankApp/MainApp/AtmApp.cs
+++ UssdBankApp/MainApp/AtmApp.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading;
+using System.Linq;
+using AtmApp.Domain.Entities;
 using UssdBankApp.Domain.Entities;
 using UssdBankApp.Domain.Enums;
 using UssdBankApp.Domain.Interfaces;
@@ -8,27 +10,39 @@ using UssdBankApp.UI;
 
 namespace UssdBankApp.MainApp 
 {
-   public class AtmApp:IUserLogin,IUserAccountActions
+   public class AtmApp:IUserLogin,IUserAccountActions,ITransaction
    {
         private List<UserAccount> userAccountList;
         private UserAccount selectedAccount;
+        private List<Transaction> _listOfTransactions;
+        private const decimal minimumKeptAmount = 500;
+        private readonly AppScreen screen;
+
+        public AtmApp()
+        {
+            screen = new AppScreen();
+        }
 
         public void Run()
         {
             AppScreen.Welcome();
             CheckUserIdAndPassword();
             AppScreen.WelcomeCustomer(selectedAccount.FullName);
-            AppScreen.DisplayAppMenu();
-            ProcessMenuOption();
+            while (true) 
+            {
+                AppScreen.DisplayAppMenu();
+                ProcessMenuOption();
+             }
         }
         public void InitializeData() 
         {
             userAccountList = new List<UserAccount>
             {
-                new UserAccount{Id=1, FullName = "Salami Waris", AccountNumber= 308088, UserId=155666, CardPin=252525, /*TotalLogin=3,*/ AccountBalance=5000.00m, IsLocked=false},
-                new UserAccount{Id=2, FullName = "Babatunde Oloja", AccountNumber=203065, UserId=157654, CardPin=252566,/* TotalLogin=3,*/ AccountBalance=10000.00m, IsLocked=false},
-                new UserAccount{Id=3, FullName = "John Salako", AccountNumber= 319765, UserId=145276, CardPin=252983,/* TotalLogin=3,*/ AccountBalance=700.00m, IsLocked=true},
+                new UserAccount{Id=1, FullName = "Salami Waris", AccountNumber= 308088, UserId=155666, CardPin=252525, AccountBalance=5000.00m, IsLocked=false},
+                new UserAccount{Id=2, FullName = "Babatunde Oloja", AccountNumber=203065, UserId=157654, CardPin=252566, AccountBalance=10000.00m, IsLocked=false},
+                new UserAccount{Id=3, FullName = "John Salako", AccountNumber= 319765, UserId=145276, CardPin=252983, AccountBalance=700.00m, IsLocked=true},
             };
+            _listOfTransactions = new List<Transaction>();
         }
         
         public void CheckUserIdAndPassword()
@@ -81,47 +95,245 @@ namespace UssdBankApp.MainApp
 
         }
 
-        private void ProcessMenuOption()
+        private void ProcessMenuoption()
         {
-            switch(Validator.Convert<int>("an option:"))
+            switch (Validator.Convert<int>("an option:"))
             {
                 case (int)AppMenu.CheckBalance:
-                    Console.WriteLine("Checking account balance...");
+                    CheckBalance();
                     break;
-                case (int)AppMenu.PlaceDepsoit:
-                    Console.WriteLine("Placing deposit...");
+                case (int)AppMenu.PlaceDeposit:
+                    PlaceDeposit();
                     break;
                 case (int)AppMenu.MakeWithdrawal:
-                    Console.WriteLine("Making withdrawal...");
+                    MakeWithDrawal();
                     break;
-                case (int)AppMenu.InternalTranfer:
-                    Console.WriteLine("Making internal transfer...");
+                case (int)AppMenu.InternalTransfer:
+                    var internalTransfer = screen.InternalTransferForm();
+                    ProcessInternalTransfer(internalTransfer);
                     break;
                 case (int)AppMenu.ViewTransaction:
-                    Console.WriteLine("Viewing transactions...");
+                    ViewTransaction();
                     break;
                 case (int)AppMenu.Logout:
-                    AppScreen.LogOutProcess();
-                    Utilities.PrintMessage("You have successfully logged out. " +
-                       "Please Protect your UserId to avoid unauthorize withdrawal.");
+                    AppScreen.LogoutProgress();
+                    Utility.PrintMessage("You have successfully logged out. Please collect " +
+                        "your ATM card.");
                     Run();
                     break;
                 default:
-                    Utilities.PrintMessage("Invalid Option.", false);
+                    Utility.PrintMessage("Invalid Option.", false);
                     break;
             }
         }
 
         public void CheckBalance()
         {
-            Utilities.PrintMessage($"Your account balance is: {selectedAccount.AccountBalance}");
+            Utilities.PrintMessage($"Your account balance is: {Utilities.FormatAmount(selectedAccount.AccountBalance)}");
         }
-         public void PlaceDeposit()
+
+        public void PlaceDeposit()
         {
+            Console.WriteLine("\nOnly multiples of 500 and 1000 naira allowed.\n");
+            var transaction_amt = Validator.Convert<int>($"amount {AppScreen.cur}");
+
+            //simulate counting
+            Console.WriteLine("\nChecking and Counting bank notes.");
+            Utility.PrintDotAnimation();
+            Console.WriteLine("");
+
+            //some gaurd clause
+            if (transaction_amt <= 0)
+            {
+                Utilities.PrintMessage("Amount needs to be greater than zero. Try again.", false); ;
+                return;
+            }
+            if (transaction_amt % 500 != 0)
+            {
+                Utilities.PrintMessage($"Enter deposit amount in multiples of 500 or 1000. Try again.", false);
+                return;
+            }
+
+            if (PreviewBankNotesCount(transaction_amt) == false)
+            {
+                Utilities.PrintMessage($"You have cancelled your action.", false);
+                return;
+            }
+
+            //bind transaction details to transaction object
+            InsertTransaction(selectedAccount.Id, TransactionType.Deposit, transaction_amt, "");
+
+            //update account balance
+            selectedAccount.AccountBalance += transaction_amt;
+
+            //print success message
+            Utilities.PrintMessage($"Your deposit of {Utility.FormatAmount(transaction_amt)} was " +
+                $"succesful.", true);
+
+
 
         }
-         public void MakeWithDrawal()
+        
+
+        public void MakeWithDrawal()
         {
+            var transaction_amt = 0;
+            int selectedAmount = AppScreen.SelectAmount();
+            if (selectedAmount == -1)
+            {
+                MakeWithDrawal();
+                return;
+            }
+            else if (selectedAmount != 0)
+            {
+                transaction_amt = selectedAmount;
+            }
+            else
+            {
+                transaction_amt = Validator.Convert<int>($"amount {AppScreen.cur}");
+            }
+
+            //input validation
+            if (transaction_amt <= 0)
+            {
+                Utilities.PrintMessage("Amount needs to be greater than zero. Try agin", false);
+                return;
+            }
+            if (transaction_amt % 500 != 0)
+            {
+                Utilities.PrintMessage("You can only withdraw amount in multiples of 500 or 1000 naira. Try again.", false);
+                return;
+            }
+            //Business logic validations
+
+            if (transaction_amt > selectedAccount.AccountBalance)
+            {
+                Utilities.PrintMessage($"Withdrawal failed. Your balance is too low to withdraw" +
+                    $"{Utilities.FormatAmount(transaction_amt)}", false);
+                return;
+            }
+            if ((selectedAccount.AccountBalance - transaction_amt) < minimumKeptAmount)
+            {
+                Utilities.PrintMessage($"Withdrawal failed. Your account needs to have " +
+                    $"minimum {Utilities.FormatAmount(minimumKeptAmount)}", false);
+                return;
+            }
+            //Bind withdrawal details to transaction object
+            InsertTransaction(selectedAccount.Id, TransactionType.Withdrawal, -transaction_amt, "");
+            //update account balance
+            selectedAccount.AccountBalance -= transaction_amt;
+            //success message
+            Utilities.PrintMessage($"You have successfully withdrawn " +
+                $"{Utilities.FormatAmount(transaction_amt)}.", true);
+        }
+
+        private bool PreviewBankNotesCount(int amount)
+        {
+            int thousandNotesCount = amount / 1000;
+            int fiveHundredNotesCount = (amount % 1000) / 500;
+
+            Console.WriteLine("\nSummary");
+            Console.WriteLine("------");
+            Console.WriteLine($"{AppScreen.cur}1000 X {thousandNotesCount} = {1000 * thousandNotesCount}");
+            Console.WriteLine($"{AppScreen.cur}500 X {fiveHundredNotesCount} = {500 * fiveHundredNotesCount}");
+            Console.WriteLine($"Total amount: {Utility.FormatAmount(amount)}\n\n");
+
+            int opt = Validator.Convert<int>("1 to confirm");
+            return opt.Equals(1);
+
+        }
+
+         public void InsertTransaction(long _UserBankAccountId, TransactionType _tranType, decimal _tranAmount, string _desc)
+        {
+            //create a new transaction object
+            var transaction = new Transaction()
+            {
+                TransactionId = Utilities.GetTransactionId(),
+                UserBankAccountId = _UserBankAccountId,
+                TransactionDate = DateTime.Now,
+                TransactionType = _tranType,
+                TransactionAmount = _tranAmount,
+                Descriprion = _desc
+            };
+
+            //add transaction object to the list
+            _listOfTransactions.Add(transaction);
+        }
+
+        public void ViewTransaction()
+        {
+            var filteredTransactionList = _listOfTransactions.Where(t => t.UserBankAccountId == selectedAccount.Id).ToList();
+            //check if there's a transaction
+            if (filteredTransactionList.Count <= 0)
+            {
+                Utilities.PrintMessage("You have no transaction yet.", true);
+            }
+            else
+            {
+                var table = new ConsoleTable("Id", "Transaction Date", "Type", "Descriptions", "Amount " + AppScreen.cur);
+                foreach (var tran in filteredTransactionList)
+                {
+                    table.AddRow(tran.TransactionId, tran.TransactionDate, tran.TransactionType, tran.Descriprion, tran.TransactionAmount);
+                }
+                //table.Options.EnableCount = false;
+                table.Write();
+                Utilities.PrintMessage($"You have {filteredTransactionList.Count} transaction(s)", true);
+            }
+        }
+
+         private void ProcessInternalTransfer(InternalTransfer internalTransfer)
+        {
+            if (internalTransfer.TransferAmount <= 0)
+            {
+                Utilities.PrintMessage("Amount needs to be more than zero. Try again.", false);
+                return;
+            }
+            //check sender's account balance
+            if (internalTransfer.TransferAmount > selectedAccount.AccountBalance)
+            {
+                Utilities.PrintMessage($"Transfer failed. You do not hav enough balance" +
+                    $" to transfer {Utilities.FormatAmount(internalTransfer.TransferAmount)}", false);
+                return;
+            }
+            //check the minimum kept amount 
+            if ((selectedAccount.AccountBalance - internalTransfer.TransferAmount) < minimumKeptAmount)
+            {
+                Utilities.PrintMessage($"Transfer faile. Your account needs to have minimum" +
+                    $" {Utilities.FormatAmount(minimumKeptAmount)}", false);
+                return;
+            }
+
+            //check reciever's account number is valid
+            var selectedBankAccountReciever = (from userAcc in userAccountList
+                                               where userAcc.AccountNumber == internalTransfer.ReciepeintBankAccountNumber
+                                               select userAcc).FirstOrDefault();
+            if (selectedBankAccountReciever == null)
+            {
+                Utilities.PrintMessage("Transfer failed. Recieber bank account number is invalid.", false);
+                return;
+            }
+            //check receiver's name
+            if (selectedBankAccountReciever.FullName != internalTransfer.RecipientBankAccountName)
+            {
+                Utilities.PrintMessage("Transfer Failed. Recipient's bank account name does not match.", false);
+                return;
+            }
+
+            //add transaction to transactions record- sender
+            InsertTransaction(selectedAccount.Id, TransactionType.Transfer, -internalTransfer.TransferAmount, "Transfered " +
+                $"to {selectedBankAccountReciever.AccountNumber} ({selectedBankAccountReciever.FullName})");
+            //update sender's account balance
+            selectedAccount.AccountBalance -= internalTransfer.TransferAmount;
+
+            //add transaction record-reciever
+            InsertTransaction(selectedBankAccountReciever.Id, TransactionType.Transfer, internalTransfer.TransferAmount, "Transfered from " +
+                $"{selectedAccount.AccountNumber}({selectedAccount.FullName})");
+            //update reciever's account balance
+            selectedBankAccountReciever.AccountBalance += internalTransfer.TransferAmount;
+            //print success message
+            Utilities.PrintMessage($"You have successfully transfered" +
+                $" {Utilities.FormatAmount(internalTransfer.TransferAmount)} to " +
+                $"{internalTransfer.RecipientBankAccountName}", true);
 
         }
 
---
+++ UssdBankApp/MainApp/ConsoleTable.cs
@@ -0,0 +1,35 @@
+﻿using UssdBankApp.Domain.Enums;
+using System;
+
+namespace UssdBankApp.App
+{
+    internal class ConsoleTable
+    {
+        private string v1;
+        private string v2;
+        private string v3;
+        private string v4;
+        private string v5;
+
+        public ConsoleTable(string v1, string v2, string v3, string v4, string v5)
+        {
+            this.v1 = v1;
+            this.v2 = v2;
+            this.v3 = v3;
+            this.v4 = v4;
+            this.v5 = v5;
+        }
+
+        public object Options { get; internal set; }
+
+        internal void AddRow(long transactionId, DateTime transactionDate, TransactionType transactionType, string descriprion, decimal transactionAmount)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void Write()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
